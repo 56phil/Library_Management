98c638f0669a2dd910bf0b6fdc112107a223294a
Phil Huffman <56phil@gmail.com>
2023-06-14 12:43:10 -0500

    Centeralizes includes into lms_project.hpp.

---
+++ lms_project.hpp
@@ -0,0 +1,26 @@
+#ifndef LMS_PROJECT_HPP
+#define LMS_PROJECT_HPP
+
+#include <_ctype.h>
+#include <algorithm>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include <cstdlib>
+#include <curses.h>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iostream>
+#include <ncurses.h>
+#include <sstream>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <unordered_map>
+#include <vector>
+
+typedef std::vector<std::string> vString;
+
+#endif // !LMS_PROJECT_HPP
